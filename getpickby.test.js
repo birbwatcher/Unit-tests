55d235ff4dba4d3b45bdf935c84a72362b5357ad
birbwatcher <d@rkmind.cc>
2022-12-13 21:49:25 +0100

    feat: pickby implemented

---
+++ getpickby.test.js
@@ -0,0 +1,10 @@
+const getPickBy = require('./task/getpickby.js');
+
+let object = { 'a': 1, 'b': '2', 'c': 3 };
+
+test(
+    'Trying to omit object',
+    () => {
+        expect(getPickBy(object, item => typeof item === 'number')).toStrictEqual({ 'a': 1, 'c': 3 });
+    },
+);
