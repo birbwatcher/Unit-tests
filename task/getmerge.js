fed5f8049a33b89d2b27ed5e5413e1167137a31b
birbwatcher <d@rkmind.cc>
2022-12-13 20:27:59 +0100

    fix: merge work for any key

--- task/getmerge.js
+++ task/getmerge.js
@@ -3,14 +3,14 @@ const { mPush } = require('./servicemethods')
 function getMerge (obj1, obj2) {
     let obj = {}
     for (let key in obj1) {
-    let result = [];
-    let len = obj1['a'].length > obj2['a'].length ? obj1['a'].length : obj2['a'].length
-    for (let i=0; i<len; i++) {
-      let x = {};
-      x = {...obj1['a'][i], ...obj2['a'][i]}
-      mPush(result, x);
-    }
-    obj[key] = result;
+        let result = [];
+        let len = obj1[key].length > obj2[key].length ? obj1[key].length : obj2[key].length
+        for (let i=0; i<len; i++) {
+            let x = {};
+            x = {...obj1[key][i], ...obj2[key][i]}
+            mPush(result, x);
+        }
+        obj[key] = result;
     return obj
     }
 }
